778f6fa4027f1f226780d1f00785124d8a210a5f
Danny Aram <dannyaram@gmail.com>
2024-03-06 20:01:43 +0200

    add useEffect and advanced useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,35 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./App.css"
 import VideoCardContainer from "./VideoCard";
 import RegisterForm from "./RegisterForm";
 
-function getVideos() {
-  return [
-    {
-      thumbnail: "https://placehold.co/320x180/darkorange/white?text=Hello+World",
-      title: "First Video Title",
-      channel: "First Channel Name",
-      views: "14",
-      timestamp: "2 hours ago",
-      channelImage: "https://placehold.co/100x100/white/black?text=HW",
-    },
-    {
-      thumbnail: "https://placehold.co/320x180/darkcyan/white?text=Video+No.2",
-      title: "Second Video Title",
-      channel: "Second Channel Name",
-      views: "27",
-      timestamp: "3 years ago",
-      channelImage: "https://placehold.co/100x100/darkgreen/black?text=V2",
-    },
-    {
-      thumbnail: "https://placehold.co/320x180/white/black?text=3rd+Video+Thumbnail",
-      title: "Third Video Title",
-      channel: "Third Channel Name",
-      views: "32",
-      timestamp: "4 months ago",
-      channelImage: "https://placehold.co/100x100/darkblue/white?text=Hi!",
-    },
-  ];
-}
-
 export default function App() {
-  const [count, setCount] = useState(0)
-  const sampleVideos = getVideos()
-  return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((previous) => previous + 1)}>
-          count is {count}
-        </button>
-        <VideoCardContainer videos={sampleVideos} />
-        <RegisterForm />
-      </div>
-    </>
-  )
+    const [count, setCount] = useState(0)
+    const [refresh, setRefresh] = useState(true);
+    const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+    useEffect(() => {
+        console.log("Adding event listener");
+        const eventID = window.addEventListener("resize", () => {
+            setWindowSize(window.innerWidth * count)
+        });
+        return () => {
+            console.log("Removing the previous listener");
+            window.removeEventListener("resize", eventID);
+        }
+    }, [count]);
+
+    return (
+        <>
+            <h3>Window size is: {windowSize}</h3>
+            <div className="card">
+                <button onClick={() => setCount((previous) => previous + 1)}>
+                    count is {count}
+                </button>
+                <button onClick={() => setRefresh(previous => !previous)}>Refresh products &#8634;</button>
+                <VideoCardContainer changeGlobalCount={setCount} refresh={refresh} />
+                <RegisterForm />
+            </div>
+        </>
+    )
 }
--- src/VideoCard.jsx
+++ src/VideoCard.jsx
@@ -1,14 +1,17 @@
-import React from "react";
-import { useState } from "react";
-import "./VideoCard.css"; // Make sure the CSS file is in the same directory
+import { useEffect, useState } from "react";
+import "./VideoCard.css";
 
-function VideoCard({ thumbnail, title, channel, views, timestamp, channelImage }) {
+
+function VideoCard({ thumbnail, title, channel, views, timestamp, channelImage, changeGlobalCount }) {
     let [updatedViews, setViews] = useState(views);
 
     return (
         <div className="videoCard">
             <img src={thumbnail} alt="Video Thumbnail" className="videoThumbnail" onClick={
-                () => setViews((previous) => Number(previous) + 1)
+                () => {
+                    setViews((previous) => Number(previous) + 1);
+                    changeGlobalCount(previous => previous + 1);
+                }
             } />
             <div className="videoInfo">
                 <div className="videoHeader">
@@ -24,23 +27,77 @@ function VideoCard({ thumbnail, title, channel, views, timestamp, channelImage }
             </div>
         </div>
     );
-};
+}
 
+const importantProducts = [
+    {
+        thumbnail: "https://placehold.co/320x180/darkorange/white?text=Hello+World",
+        title: "First Video Title",
+        channel: "First Channel Name",
+        views: "14",
+        timestamp: "2 hours ago",
+        channelImage: "https://placehold.co/100x100/white/black?text=HW",
+    },
+    {
+        thumbnail: "https://placehold.co/320x180/darkcyan/white?text=Video+No.2",
+        title: "Second Video Title",
+        channel: "Second Channel Name",
+        views: "27",
+        timestamp: "3 years ago",
+        channelImage: "https://placehold.co/100x100/darkgreen/black?text=V2",
+    },
+    {
+        thumbnail: "https://placehold.co/320x180/white/black?text=3rd+Video+Thumbnail",
+        title: "Third Video Title",
+        channel: "Third Channel Name",
+        views: "32",
+        timestamp: "4 months ago",
+        channelImage: "https://placehold.co/100x100/darkblue/white?text=Hi!",
+    },
+];
 
-export default function VideoCardContainer({ videos }) {
+async function getVideos() {
+    console.log("Getting data from the WEB");
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    for (let item of data) {
+        item.thumbnail = item.image;
+        item.channel = item.category;
+        item.views = item.id;
+        item.timestamp = "2 Days ago";
+        item.channelImage = item.image;
+    }
+    return data;
+}
+
+export default function VideoCardContainer({ changeGlobalCount, refresh }) {
+    const [videos, setVideos] = useState(importantProducts);
+
+    useEffect(() => {
+        getVideos().then((videosFromWeb) => {
+            const start = Math.floor(Math.random() * 10);
+            const end = start + 3;
+            const threeProducts = videosFromWeb.slice(start, end)
+            setVideos(threeProducts);
+        });
+    }, [refresh]);
+
+    console.log("This video container is rendered");
+
     return (
         <div className="videoCardContainer">
             {videos.map((video, index) => (
                 <VideoCard
-                    key={index}
+                    key={video.title}
                     thumbnail={video.thumbnail}
                     title={video.title}
                     channel={video.channel}
                     views={video.views}
                     timestamp={video.timestamp}
                     channelImage={video.channelImage}
+                    changeGlobalCount={changeGlobalCount}
                 />
             ))}
         </div>
     );
-};
+}
